8d4a675a90385d5e300514a7a45f441e42301077
harshsingh9898 <73526355+harshsingh9898@users.noreply.github.com>
2022-06-18 04:36:19 +0530

    Add files via upload

---
+++ Bubble.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    public class Bubble
+    {
+        public static void bubble()
+        {
+            {
+                int[] arr = { 25, 41, 3, 7, 9, 15, 63, 108, 47, 84, 1104, 37 };
+                int n = arr.Length;
+                for (int i = 0; i < n - 1; i++)
+                    for (int j = 0; j < n - i - 1; j++)
+                        if (arr[j] > arr[j + 1])
+                        {
+                            // swap temp and arr[i]
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                for (int i = 0; i < n; ++i)
+                    Console.WriteLine(arr[i] + " ");
+            }
+                
+                
+            
+
+        }
+    }
+}
---
+++ insertion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    public class insertion
+    {
+        public static void intsort()
+        {
+            int val, flag;
+            int[] arr = { 25, 41, 3, 7, 9, 15, 63, 108, 47, 84, 1104, 37 };
+            Console.WriteLine("Unsorted array is: ");
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine(arr[i]);
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                val = arr[i];
+                flag = 0;
+
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                        flag = 1;
+                }
+
+            }
+            Console.WriteLine("Sorted array is: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
